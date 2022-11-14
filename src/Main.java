51c410375dfab3802250607ab97b46065140b2f1
Vitisadi <82157788+Vitisadi@users.noreply.github.com>
2022-11-14 15:17:50 -0500

    added mouse location + draw board

--- src/Main.java
+++ src/Main.java
@@ -10,34 +10,29 @@ import java.util.ArrayList;
 
 
 public class Main extends PApplet {
-    ArrayList<ArrayList<Piece>> board = new ArrayList<ArrayList<Piece>>();
+    ArrayList<ArrayList<Integer>> board = new ArrayList<ArrayList<Integer>>(); //0 = empty / 1 = player / 2 = computer
+    PImage board_image;
 
 
 
     public static PApplet processing;
     public void settings () {
-        size(800, 800);
+        size(750, 650);
     }
 
     public void setup() {
         processing = this;
+        setupBoard();
     }
 
     public void draw() {
-        background(255,0,0);
+        background(50, 162, 168);
         drawBoard();
-        for (ArrayList<Piece> yList : board) {
-            for (Piece piece : yList) {
-                if (piece != null ){
-                    piece.display();
-                }
-            }
-        }
     }
 
     public void mousePressed(){
-        Coordinate coordinate = findMouseLocation();
-        
+        int coordinate = findMouseLocation();
+        print(coordinate);
     }
 
     public static void main(String[] args) {
@@ -45,17 +40,44 @@ public class Main extends PApplet {
     }
 
     void drawBoard(){
-        
+        for (int y = 0; y < board.size(); y++) {
+            ArrayList<Integer> row = board.get(y);
+            for (int x = 0; x < row.size(); x++) {
+                int value = row.get(x);
+                if (value == 0){ //Neutral
+                    fill(255);
+                } else if(value == 1){ //Player
+                    fill(218, 224, 20);
+                } else { //Computer
+                    fill(181, 11, 39);
+                }
+                ellipse(75 + x * 100, 75 + y * 100, 90, 90);
+            }
+
+            //Tint here
+        }
+    }
+
+    void setupBoard(){
+        for(int row = 0; row < 6; row++){
+            ArrayList<Integer> rowList  = new ArrayList<>();
+            for(int column = 0; column < 7; column++){
+                rowList.add(0);
+            }
+            board.add(rowList);
+        }
     }
 
 
-    Coordinate findMouseLocation(){ //ar[0] = x & ar[1] = y
-        Coordinate coordinate = new Coordinate(mouseX / 100, 7 - mouseY / 100);
-
-        if (coordinate.x >7 || coordinate.y < 0) {
-            return null;
+    Integer findMouseLocation(){ //75 - 45 + 100 * (0), 75 + 45 + 100 * 0
+        for(int column = 0; column < 7; column++){
+            int lowBound = 75 - 50 + 100 * column;
+            int highBound = 75 + 50 + 100 * column;
+            if (mouseX > lowBound && mouseX < highBound){
+                return column;
+            }
         }
-        return coordinate;
+        return -1;
     }
 
     
